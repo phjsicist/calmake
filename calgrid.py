2baaa275752b5a138eff8a352d12e8cc3bba76c2
Janis Schertenleib <janis.schertenleib@outlook.com>
2025-03-09 10:13:57 +0100

    Implement automatic dates grid formatting

--- calgrid.py
+++ calgrid.py
@@ -1,7 +1,11 @@
 import argparse
+import calendar
+import datetime
 import os
 
 import jinja2
+
+from constants import DAYS1, DAYS2, MONTHS
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
@@ -21,7 +25,26 @@ latex_jinja_env = jinja2.Environment(
 	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
 )
 
+language = 'fr'
+
+tex_kwargs = {
+	'month': 2,
+    'year': 2024,
+	'weekdays_line': ' & '.join(DAYS2[language]) + '\\\\',
+    'dates_grid': '1 & 2 & 3 & 4 & 5 & 6 & 7 \\\\ 8 & 9 & 10 & 11 & 12 & 13 & 14 \\\\ 15 & 16 & 17 & 18 & 19 & 20 & 21 \\\\ 22 & 23 & 24 & 25 & 26 & 27 & 28 \\\\ 29 & 30 & 31 & & & &',
+}
+
+first_weekday = datetime.date(tex_kwargs['year'], tex_kwargs['month'], 1).weekday()
+length_of_month = calendar.monthrange(tex_kwargs['year'], tex_kwargs['month'])[1]
+dates_list = [''] * first_weekday + [str(d) for d in range(1, length_of_month + 1)]
+
+tex_kwargs['dates_grid'] = ''
+for i in range(0, len(dates_list), 7):
+	tex_kwargs['dates_grid'] += ' & '.join(dates_list[i:i+7]) + ' \\\\ '
+
+tex_kwargs['month_name'] = MONTHS[language][tex_kwargs['month'] - 1]
+
 template = latex_jinja_env.get_template('calgrid.tex.jinja')
 
-with open(args.filename, 'w') as f:
-    f.write(template.render(month='Mar'))
+with open(args.filename, 'w', encoding='utf-8') as f:
+    f.write(template.render(**tex_kwargs))
--- constants.py
+++ constants.py
@@ -1,14 +1,20 @@
-DAYS1 = {'de': ['M', 'D', 'M', 'D', 'F', 'S', 'S'],
-        'en': ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-        'fr': ['L', 'M', 'M', 'J', 'V', 'S', 'D'],
+DAYS1 = {
+    'ch': ['M', 'Z', 'M', 'D', 'F', 'S', 'S'],
+    'de': ['M', 'D', 'M', 'D', 'F', 'S', 'S'],
+    'en': ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    'fr': ['L', 'M', 'M', 'J', 'V', 'S', 'D'],
 }
 
-DAYS2 = {'de': ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
-        'en': ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-        'fr': ['Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa', 'Di'],
+DAYS2 = {
+    'ch': ['Mä', 'Zi', 'Mi', 'Do', 'Fr', 'Sa', 'Su'],
+    'de': ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
+    'en': ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+    'fr': ['Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa', 'Di'],
 }
 
-MONTHS = {'de': ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
-          'en': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-          'fr': ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Déc'],
+MONTHS = {
+    'ch': ['Jan', 'Feb', 'Mär', 'Apr', 'Mei', 'Jun', 'Jul', 'Oug', 'Sep', 'Okt', 'Nov', 'Dez'],
+    'de': ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+    'en': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    'fr': ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Déc'],
 }
